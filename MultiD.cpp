3d4d5b16a9803bad4bd2445891afb7bcbc3183f1
Harshit Shandil <harshitshandil65@gmail.com>
2025-01-12 00:15:08 +0530

    MultiD.cpp modified

--- MultiD.cpp
+++ MultiD.cpp
@@ -23,23 +23,5 @@ int main()
     }
 
 
-    // int i=0; 
-    // int n=0;
-    // cout<< "Enter n" <<endl;
-    // cin>>n;
-    // int arr[n]={0};
-    
-    // cout<<"Insert values :";
-    // for (i=0; i<n; i++)
-    // {
-    //     cin>>arr[i];
-    // }
-    
-    // cout<< "Printing inserted values" <<endl;
-    //  for (int i=0; i<n; i++)
-    // {
-    //     cout<< arr[i] <<" ";
-    // }
-
     return 0;
 }

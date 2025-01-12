ce2e7d845e899c52d0a765383768fec20a9e2545
Harshit Shandil <harshitshandil65@gmail.com>
2025-01-12 15:34:42 +0530

    secondlarg also

--- largest_element.cpp
+++ largest_element.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
 using namespace std;
-int main()
 
-{     
-    int arr[4]={10,100,20 ,30};
+int main() {
+    int arr[4] = {10, 100, 20, 30};
 
-    int second_largest=-1; int largest=-1;
+    int largest = -1;
+    int second_largest = -1;
 
-    for(int i=0; i<4; i++)
-    {
-        cout<<arr[i]<<" ";
+    // Print the array elements
+    for (int i = 0; i < 4; i++) {
+        cout << arr[i] << " ";
     }
-    
-    
-    for(int i=0; i<4; i++)
-    {
-        if(arr[i]>largest)
-        {
-            largest=arr[i];
+    cout << endl;
+
+    // Find the largest element
+    for (int i = 0; i < 4; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
         }
     }
+    cout << "Largest: " << largest << endl;
 
-    cout<<endl<<"Largest:"<<largest;
-    
-    // int second_largest=0;
-    for(int i; i<4; i++)
-    {
-        if(arr[i]>largest)
-        {
-            second_largest=largest;
-            largest=arr[i];
-        }
-        else if(arr[i] < largest && arr[i] > second_largest) {
+    // Find the second-largest element
+    for (int i = 0; i < 4; i++) {
+        if (arr[i] < largest && arr[i] > second_largest) {
             second_largest = arr[i];
         }
     }
-    
 
-    cout<<endl<<"secondd largest:"<<second_largest;
-
+    // Print the second-largest element
+    if (second_largest != -1) {
+        cout << "Second Largest: " << second_largest << endl;
+    } else {
+        cout << "No second-largest element found." << endl;
+    }
 
     return 0;
 }
